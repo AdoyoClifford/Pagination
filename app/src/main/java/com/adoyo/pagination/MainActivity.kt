0ed40686833aa2842d71ab53c36637e619bc1265
Adoyo Clifford <adoyoclifford@gmail.com>
2023-05-07 16:52:20 +0300

    initial commit

--- app/src/main/java/com/adoyo/pagination/MainActivity.kt
+++ app/src/main/java/com/adoyo/pagination/MainActivity.kt
@@ -10,8 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.adoyo.pagination.presentattion.BeerScreen
+import com.adoyo.pagination.presentattion.BeerViewModel
 import com.adoyo.pagination.ui.theme.PaginationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +28,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val viewModel = hiltViewModel<BeerViewModel>()
+                    val beer = viewModel.beerPagingFlow.collectAsLazyPagingItems()
+                    BeerScreen(beers = beer)
                 }
             }
         }
---
+++ app/src/main/java/com/adoyo/pagination/PagingApp.kt
@@ -0,0 +1,7 @@
+package com.adoyo.pagination
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PagingApp: Application()
