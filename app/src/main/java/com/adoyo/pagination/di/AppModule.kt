db935e78a671f3ec798a646bcb5005121c7379a5
Adoyo Clifford <adoyoclifford@gmail.com>
2023-05-07 17:03:13 +0300

    initial commit

---
+++ app/src/main/java/com/adoyo/pagination/di/AppModule.kt
@@ -0,0 +1,55 @@
+package com.adoyo.pagination.di
+
+import android.content.Context
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.room.Room
+import com.adoyo.pagination.data.local.BeerDatabase
+import com.adoyo.pagination.data.local.BeerEntity
+import com.adoyo.pagination.data.remote.BeerApi
+import com.adoyo.pagination.data.remote.BeerRemoteMediator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@OptIn(ExperimentalPagingApi::class)
+@Module
+@InstallIn(Singleton::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideBeerApi(): BeerApi {
+        return Retrofit.Builder()
+            .baseUrl(BeerApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): BeerDatabase {
+        return Room.databaseBuilder(context, BeerDatabase::class.java, "beers.db").build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePager(beerDb: BeerDatabase,beerApi: BeerApi): Pager<Int, BeerEntity> {
+        return Pager(
+            config = PagingConfig(pageSize = 20),
+            remoteMediator = BeerRemoteMediator(
+                beerApi = beerApi, beerDb = beerDb
+            ),
+            pagingSourceFactory = {
+                beerDb.dao.pagingSource()
+            }
+        )
+    }
+}
